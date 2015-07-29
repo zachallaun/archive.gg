b471218a50216b10b619ea465924bfb03e275a53
Zachary Allaun <zach.allaun@gmail.com>
2015-07-29 00:01:33 -0400

    Parse x-www-form-urlencoded bodies

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,7 @@ import config from '../config';
 import actions from './routes/index';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 export default function api() {
